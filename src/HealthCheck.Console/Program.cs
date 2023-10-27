81160148e1d865548b27ae09a5c81bb365e18af7
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2023-10-27 18:19:19 +0000

    Create health check project using nativa aot

---
+++ src/HealthCheck.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿var client = new HttpClient();
+
+bool isHealthy = false;
+try
+{
+    var response = await client.GetAsync(args[0]);
+    if (response.IsSuccessStatusCode)
+        isHealthy = true;
+}
+catch
+{
+    isHealthy = false;
+}
+
+if (isHealthy)
+    return 0;
+else
+    return 1;
