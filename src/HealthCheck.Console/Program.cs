898047096f5174340e7e9289cd2891812950aa26
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2023-10-30 15:57:29 +0000

    Make HealthCheck.Console is testable

--- src/HealthCheck.Console/Program.cs
+++ src/HealthCheck.Console/Program.cs
@@ -1,19 +1,27 @@
-﻿var client = new HttpClient();
+namespace HealthCheck.Console;
 
-bool isHealthy = false;
-try
+internal class Program
 {
-    var uriBuilder = new UriBuilder(args[0]);
-    var response = await client.GetAsync(uriBuilder.Uri);
-    if (response.IsSuccessStatusCode)
-        isHealthy = true;
-}
-catch
-{
-    isHealthy = false;
-}
+    internal static async Task<int> Main(string[] args)
+    {
+        var client = new HttpClient();
 
-if (isHealthy)
-    return 0;
-else
-    return 1;
+        bool isHealthy = false;
+        try
+        {
+            var uriBuilder = new UriBuilder(args[0]);
+            var response = await client.GetAsync(uriBuilder.Uri);
+            if (response.IsSuccessStatusCode)
+                isHealthy = true;
+        }
+        catch
+        {
+            isHealthy = false;
+        }
+
+        if (isHealthy)
+            return 0;
+        else
+            return 1;
+    }
+}
